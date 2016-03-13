ff79a80c0b516ab97dcff7090de0fe54d09cc020
zolcsieles <zolcsieles@gmail.com>
2016-03-13 22:44:26 +0100

    Rotating cube...

--- VRTestApp/VRTestApp.cpp
+++ VRTestApp/VRTestApp.cpp
@@ -6,10 +6,20 @@
 #include "ERR.h"
 #include "GL.h"
 
+#include <math.h>
+
 bool runing = true;
 bool stereoRenderInited = false;
 bool stereoRenderUsed = false;
 int actQuad = 0;
+float dif = -0.05f;
+
+int dispWidth = 800;
+int dispHeight = 600;
+int iEyeWidth;
+int iEyeHeight;
+
+float rot = 0.0f;
 
 enum ZLS_Eye {
 	Eye_Center,
@@ -17,7 +27,7 @@ enum ZLS_Eye {
 	Eye_Right
 };
 
-extern void InitStereoRender();
+extern void InitStereoRender(int eyeWidth, int eyeHeight);
 extern void Render(ZLS_Eye act);
 extern void setRenderFrame(void (render)(ZLS_Eye), bool isStereo);
 
@@ -25,9 +35,6 @@ void MyExit()
 {
 	shutdownGX();
 	shutdownVR();
-
-	printf("Press ENTER to exit.\n");
-	getchar();
 }
 
 bool swapEyes;
@@ -49,21 +56,29 @@ void Events()
 		}
 		if (e.type == SDL_KEYDOWN)
 		{
-			switch (e.key.keysym.sym)
+			switch (e.key.keysym.scancode)
 			{
-			case VK_SPACE:
+			case SDL_SCANCODE_SPACE:
 				swapEyes ^= true;
 				break;
-			case '1':
-			case '2':
-			case '3':
-				actQuad = e.key.keysym.sym - '1';
+			case SDL_SCANCODE_1:
+			case SDL_SCANCODE_2:
+			case SDL_SCANCODE_3:
+				actQuad = e.key.keysym.scancode - SDL_SCANCODE_1;
 				break;
-			case 'w':
+			case SDL_SCANCODE_KP_PLUS:
+				dif += 0.01f;
+				break;
+			case SDL_SCANCODE_KP_MINUS:
+				dif -= 0.01f;
+				break;
+			case SDL_SCANCODE_Q:
+				break;
+			case SDL_SCANCODE_W:
 				stereoRenderUsed ^= true;
 				if (stereoRenderUsed && !stereoRenderInited)
 				{
-					InitStereoRender();
+					InitStereoRender(dispWidth, dispHeight);
 				}
 				setRenderFrame(Render, stereoRenderUsed);
 				break;
@@ -133,6 +148,58 @@ void symfrustum(T _near, T _far, T _top, T _right, T(&m)[4][4])
 }
 
 template<typename T>
+void rotatez(T rot, T(&m)[4][4])
+{
+	float r = rot * M_PI / 180.0f;
+	m[0][0] = cos(r);
+	m[1][1] = m[0][0];
+	m[2][2] = 1.0f;
+	m[3][3] = 1.0f;
+	m[1][0] = sin(r);
+	m[0][1] = -m[1][0];
+};
+
+template<typename T>
+void rotatey(T rot, T(&m)[4][4])
+{
+	float r = rot * M_PI / 180.0f;
+	m[0][0] = cos(r);
+	m[1][1] = 1.0f;
+	m[2][2] = m[0][0];
+	m[3][3] = 1.0f;
+
+	m[2][0] = sin(r);
+	m[0][2] = -m[2][0];
+};
+
+template<typename T>
+void rotatex(T rot, T(&m)[4][4])
+{
+	float r = rot * M_PI / 180.0f;
+	m[0][0] = 1.0f;
+	m[1][1] = cos(r);
+	m[2][2] = m[1][1];
+	m[3][3] = 1.0f;
+
+	m[1][2] = sin(r);
+	m[2][1] = -m[1][2];
+};
+
+template<typename T>
+void translate(T x, T y, T z, T(&m)[4][4])
+{
+	m[0][0] = 1.0f;
+	m[1][1] = 1.0f;
+	m[2][2] = 1.0f;
+	m[3][3] = 1.0f;
+
+	m[3][0] = x;
+	m[3][1] = y;
+	m[3][2] = z;
+};
+
+
+template<typename T>
 void identity(T(& m)[4][4])
 {
 	m[0][0] = m[1][1] = m[2][2] = m[3][3] = 1.0;
@@ -140,9 +207,6 @@ void identity(T(& m)[4][4])
 }
 
 //Shaders and Programs
-const int dispWidth = 800;
-const int dispHeight = 600;
-
 GLuint vs, fs, simple;
 GLuint modelMatIdx, viewMatIdx, projMatIdx;
 GLuint vertArrayObj;
@@ -172,29 +236,37 @@ const int N_STEREO = 2;
 GLuint eyeRB[N_STEREO];
 GLuint eyeDepth[N_STEREO];
 GLuint eyeTex[N_STEREO];
-GLuint quadArray, quadVertexBuffer, quadProgram[QS_COUNT], quadVS, quadFS[QS_COUNT], quadTex[QS_COUNT][N_STEREO], swapEyesIdx[QS_COUNT];
+GLuint quadArray, quadVertexBuffer, quadProgram[QS_COUNT], quadVS, quadFS[QS_COUNT], quadTex[QS_COUNT][N_STEREO], swapEyesIdx[QS_COUNT]/*, quadMultiplier[QS_COUNT]*/;
 
 void Render(ZLS_Eye act)
 {
 	gl::glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	gl::glUseProgram(simple);
 
+	const float dist = 20.0f;
 	switch (act)
 	{
 	case Eye_Left:
-		gl::glUniform3f(faceColorIdx, 1.0f, 0.0f, 0.0f); break;
+		gl::glUniform3f(faceColorIdx, 1.0f, 0.0f, 0.0f); 
+		translate(-dif, 0.0f, -dist, viewMat);
+		break;
 	case Eye_Right:
-		gl::glUniform3f(faceColorIdx, 0.0f, 0.0f, 1.0f); break;
+		gl::glUniform3f(faceColorIdx, 0.0f, 0.0f, 1.0f);
+		translate(+dif, 0.0f, -dist, viewMat);
+		break;
 	case Eye_Center: default:
-		gl::glUniform3f(faceColorIdx, 0.0f, 1.0f, 0.0f); break;
+		gl::glUniform3f(faceColorIdx, 0.0f, 1.0f, 0.0f); 
+		translate(0.0f, 0.0f, -dist, viewMat);
+		break;
 	}
+	gl::glUniform3f(faceColorIdx, 0.5f, 0.0f, 0.0f);
 
 	gl::glUniformMatrix4fv(projMatIdx, 1, GL_FALSE, (GLfloat*)projMat);
 	gl::glUniformMatrix4fv(viewMatIdx, 1, GL_FALSE, (GLfloat*)viewMat);
 	gl::glUniformMatrix4fv(modelMatIdx, 1, GL_FALSE, (GLfloat*)modelMat);
 
 	gl::glBindVertexArray(vertArrayObj);
-	gl::glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+	gl::glDrawElements(GL_TRIANGLES, 3*2*2*3, GL_UNSIGNED_INT, 0);
 	gl::glBindVertexArray(0);
 
 	gl::glUseProgram(0);
@@ -207,12 +279,12 @@ void stereoRenderFrame()
 {
 	//Render left
 	gl::glBindFramebuffer(GL_FRAMEBUFFER, eyeRB[0]);
-	gl::glViewport(0, 0, dispWidth, dispHeight);
+	gl::glViewport(0, 0, iEyeWidth, iEyeHeight);
 	renderSubFrame(Eye_Left);
 
 	//Render right
 	gl::glBindFramebuffer(GL_FRAMEBUFFER, eyeRB[1]);
-	gl::glViewport(0, 0, dispWidth, dispHeight);
+	gl::glViewport(0, 0, iEyeWidth, iEyeHeight);
 	renderSubFrame(Eye_Right);
 
 	//Render screen
@@ -223,7 +295,7 @@ void stereoRenderFrame()
 	gl::glUseProgram(quadProgram[actQuad]);
 
 	gl::glProgramUniform1f(quadProgram[actQuad], swapEyesIdx[actQuad], swapEyes^(swapEyesByLine&&actQuad==QS_INTERLACED) ? 1.0f : 0.0f);
-
+	
 	gl::glActiveTexture(GL_TEXTURE0);
 	gl::glBindTexture(GL_TEXTURE_2D, eyeTex[0]);
 	gl::glProgramUniform1i(quadProgram[actQuad], quadTex[actQuad][0], 0);
@@ -322,17 +394,17 @@ void CreateAndLinkProgram(const char* vertexShaderFile, const char* fragShaderFi
 	*prog = LinkProgram(*vertShader, *fragShader);
 }
 
-bool InitRenderBuffer(GLuint& rb, GLuint& tx, GLuint& zb)
+bool InitRenderBuffer(GLuint& rb, GLuint& tx, GLuint& zb, int eyeWidth, int eyeHeight)
 {
 	gl::glBindFramebuffer(GL_FRAMEBUFFER, rb);
 
 	gl::glBindTexture(GL_TEXTURE_2D, tx);
-	gl::glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, dispWidth, dispHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+	gl::glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, eyeWidth, eyeHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
 	gl::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	gl::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
 	gl::glBindRenderbuffer(GL_RENDERBUFFER, zb);
-	gl::glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, dispWidth, dispHeight);
+	gl::glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, eyeWidth, eyeHeight);
 	gl::glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, zb);
 
 	gl::glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, tx, 0);
@@ -342,10 +414,14 @@ bool InitRenderBuffer(GLuint& rb, GLuint& tx, GLuint& zb)
 
 	if (gl::glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
 		return false;
+
+	iEyeWidth = eyeWidth;
+	iEyeHeight = eyeHeight;
+
 	return true;
 }
 
-void InitStereoRender()
+void InitStereoRender(int eyeWidth, int eyeHeight)
 {
 	if (stereoRenderInited)
 		return;
@@ -355,7 +431,7 @@ void InitStereoRender()
 	gl::glGenRenderbuffers(2, eyeDepth);
 	for (int i = 0; i < 2; ++i)
 	{
-		if (!InitRenderBuffer(eyeRB[i], eyeTex[i], eyeDepth[i]))
+		if (!InitRenderBuffer(eyeRB[i], eyeTex[i], eyeDepth[i], eyeWidth, eyeHeight))
 		{
 			ErrorExit("Unable to create renderbuffer %i", i);
 		}
@@ -379,7 +455,7 @@ void InitStereoRender()
 	gl::glBufferData(GL_ARRAY_BUFFER, sizeof(quadBufferData), quadBufferData, GL_STATIC_DRAW);
 	gl::glEnableVertexAttribArray(0); //Matches layout (location = 0)
 	gl::glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3 + sizeof(float) * 2, 0);
-	gl::glEnableVertexAttribArray(1);
+	gl::glEnableVertexAttribArray(1); //Matches layout (location = 1)
 	gl::glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 3 + sizeof(float) * 2, (void*)(3 * sizeof(float)));
 
 	quadVS = LoadShader(QuadVShaderFile, GL_VERTEX_SHADER);
@@ -392,6 +468,7 @@ void InitStereoRender()
 		quadTex[i][0] = gl::glGetUniformLocation(quadProgram[i], "texLeft");
 		quadTex[i][1] = gl::glGetUniformLocation(quadProgram[i], "texRight");
 		swapEyesIdx[i] = gl::glGetUniformLocation(quadProgram[i], "swapEyes");
+		//quadMultiplier[i] = gl::glGetUniformLocation(quadProgram[i], "multiplier");
 	}
 	
 	stereoRenderInited = true;
@@ -409,33 +486,67 @@ void InitGeometry()
 		float x, y, z;
 	};
 
-	const int nTriangleVertices = 3;
-	Vertex v_triangle[nTriangleVertices] =
+#ifdef TRIANGLE
+	const int nVertices = 3;
+	Vertex v_buffer[nVertices] =
 	{
 		{-0.25f,  0.5f, -1.0f },
 		{-0.5f, -0.5f, -1.0f },
 		{ 0.5f, -0.5f, -1.0f }
 	};
 
-	const int nTriangleIndices = 3;
-	unsigned int i_triangle[nTriangleIndices] =
+	const int nIndices = 3;
+	unsigned int i_buffer[nIndices] =
 	{
 		0, 1, 2
 	};
+#else
+	const int nVertices = 8;
+	Vertex v_buffer[nVertices] =
+	{
+		{ -5.0f, -5.0f, -5.0f },
+		{ +5.0f, -5.0f, -5.0f },
+		{ -5.0f, +5.0f, -5.0f },
+		{ +5.0f, +5.0f, -5.0f },
+		{ -5.0f, -5.0f, +5.0f },
+		{ +5.0f, -5.0f, +5.0f },
+		{ -5.0f, +5.0f, +5.0f },
+		{ +5.0f, +5.0f, +5.0f }
+	};
+
+	const int nIndices = 3*2*2*3;
+	unsigned int i_buffer[] =
+	{
+		0, 1, 2,
+		1, 2, 3,
+		4, 5, 6,
+		5, 6, 7,
+
+		0, 1, 4,
+		1, 4, 5,
+		2, 3, 6,
+		3, 6, 7,
+
+		0, 2, 4,
+		2, 4, 6,
+		1, 3, 5,
+		2, 5, 7,
+	};
+#endif
 
 	gl::glBindBuffer(GL_ARRAY_BUFFER, vertBuffer);
-	gl::glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)*nTriangleVertices, v_triangle, GL_STATIC_DRAW);
+	gl::glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)*nVertices, v_buffer, GL_STATIC_DRAW);
 	gl::glEnableVertexAttribArray(0); //Matches layout (location = 0)
 	gl::glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
 
 	gl::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-	gl::glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*nTriangleIndices, i_triangle, GL_STATIC_DRAW);
+	gl::glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*nIndices, i_buffer, GL_STATIC_DRAW);
 	gl::glBindVertexArray(0); //VertexArrayObject
 }
 
 void InitShaders()
 {
-	CreateAndLinkProgram("Data/Shaders/simple.vs", "Data/Shaders/simple.fs", &fs, &vs, &simple);
+	CreateAndLinkProgram("Data/Shaders/simple_shaded.vs", "Data/Shaders/simple_shaded.fs", &fs, &vs, &simple);
 	modelMatIdx = gl::glGetUniformLocation(simple, "modelMat");
 	viewMatIdx = gl::glGetUniformLocation(simple, "viewMat");
 	projMatIdx = gl::glGetUniformLocation(simple, "projMat");
@@ -456,7 +567,13 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 	setRenderFrame(Render, useVr);
 
-	if (!initGX("Punci", PosCenter, PosCenter, dispWidth, dispHeight, GX_OGL))
+	int PosCenterDisplay = SDL_WINDOWPOS_CENTERED_DISPLAY(1);
+#ifdef FULLSCREEN
+	dispWidth = 1920;
+	dispHeight = 1080;
+#endif
+
+	if (!initGX("Punci", PosCenterDisplay, PosCenterDisplay, dispWidth, dispHeight, GX_OGL))
 	{
 		ErrorExit("Unable to initialize GX system!\n");
 	}
@@ -481,7 +598,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	InitGeometry();
 
 	//Matrices
-	symfrustum(1.0f, 100.0f, 1.0f, 1.0f, projMat);
+	symfrustum(1.0f, 100.0f, 1.0f, float(dispWidth)/float(dispHeight), projMat);
 	identity(viewMat);
 	identity(modelMat);
 
@@ -489,13 +606,27 @@ int _tmain(int argc, _TCHAR* argv[])
 	bool useStereo = useVr;
 	if (useStereo)
 	{
-		InitStereoRender();
+		InitStereoRender(dispWidth, dispHeight);
 	}
 
+	float t = 0.0f;
+
+	gl::glEnable(GL_DEPTH_TEST);
+	gl::glFrontFace(GL_CW);
+	gl::glCullFace(GL_FRONT_FACE);
+
+	float tick0, tick1, dt;
+
+	tick0 = tick1 = GetTickCount() * (1.0f / 1000.0f);
 	while (runing)
 	{
+		tick0 = tick1;
+		tick1 = GetTickCount() * (1.0f / 1000.0f);
+		dt = tick1 - tick0;
 		Events();
 		renderFrame();
+		rotatex(t, modelMat);
+		t += 45*dt;
 	}
 
 	return 0;
