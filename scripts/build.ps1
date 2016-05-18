4ddb6df8675bbf00ded25484125c9883e2f4f349
zolcsieles <zolcsieles@gmail.com>
2016-05-18 02:56:13 +0200

    [cov] covfile

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,3 +1,7 @@
+# Test
+#$env:PLATFORM="Win32"
+
+#Build command and arguments
 $buildCmd = "C:\Program Files (x86)\MSBuild\12.0\bin\msbuild.exe"
 
 $buildArgs = @(
@@ -8,7 +12,7 @@ $buildArgs = @(
 if ($env:APPVEYOR_REPO_COMMIT_MESSAGE -and ($env:APPVEYOR_REPO_COMMIT_MESSAGE).Contains("[cov]"))
 {
 	& cov-build --dir cov-int $buildCmd $buildArgs
-	$covfile = cov-vrtestapp-$env:PLATFORM.zip
+	$covfile = "cov-vrtestapp-$env:PLATFORM.zip"
 	& 7z a $covfile cov-int
 	& curl.exe --silent --form token=p0P3q01mzsV165C6db1bbQ --form email=zolcsieles@gmail.com --form file=@$covfile --form version="$env:APPVEYOR_BUILD_VERSION" --form description="$env:PLATFORM-$env:APPVEYOR_REPO_COMMIT_MESSAGE" https://scan.coverity.com/builds?project=zolcsieles%2FVRTestApp
 }
